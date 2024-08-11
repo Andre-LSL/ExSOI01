fbde0a68644bfa29ce8826487edd8c66e4f03b8a
Andre-LSL <andreluizsilvalm@gmail.com>
2024-08-11 16:52:00 -0300

    Resolução - Ex01

--- Exercicio01/src/controller/Metodos.java
+++ Exercicio01/src/controller/Metodos.java
@@ -6,4 +6,17 @@ public class Metodos {
         super();
     }
 
+    public void vetCalculo (int[] vetor) {
+        double tiStart, tiFinal, tiTotal;
+        int size = vetor.length;
+        tiStart = System.nanoTime();
+
+        for (int cont = 0; cont < size; cont++) {
+            vetor[cont] = 0;
+        }
+
+        tiFinal = System.nanoTime();
+        tiTotal = (tiFinal-tiStart)/ (Math.pow(10, 9));
+        System.out.println("\nTotal time: " + tiTotal);
+    }
 }
--- Exercicio01/src/view/Principal.java
+++ Exercicio01/src/view/Principal.java
@@ -1,7 +1,15 @@
 package view;
-
+import controller.Metodos;
 public class Principal {
     public static void main(String[] args) {
-    
+        Metodos metdos = new Metodos();
+
+        int[] vt1000 = new int[1000];
+        int[] vt10000 = new int[10000];
+        int[] vt100000 = new int[100000];
+        // Enviar valores para classe metodos
+        metdos.vetCalculo(vt1000);
+        metdos.vetCalculo(vt10000);
+        metdos.vetCalculo(vt100000);
     }
 }
