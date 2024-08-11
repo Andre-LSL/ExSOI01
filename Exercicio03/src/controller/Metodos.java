699d7f1b688e7df22ef0c24918382a4cd3b4fc81
Andre-LSL <andreluizsilvalm@gmail.com>
2024-08-11 18:38:27 -0300

    Resolução - Ex03

--- Exercicio03/src/controller/Metodos.java
+++ Exercicio03/src/controller/Metodos.java
@@ -1,7 +1,42 @@
 package controller;
-
+import javax.swing.JOptionPane;
 public class Metodos {
     public Metodos(){
         super();
+    }
+    
+    public int limiteVt () {
+        int valor;
+
+        while (true) {
+            valor = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor limite:"));
+            if (valor < 101 && valor > 0) {
+                break;
+            } else {
+                JOptionPane.showMessageDialog(null, "Valor inválido, digite novamente.");;
+            }
+        }
+        return valor;
+
+    }
+
+    public int[] carregarVt (int[] vt) {
+        int vtTamanho = vt.length;
+        for(int ctdr = 0; ctdr < vtTamanho; ctdr++) {
+            vt[ctdr] = Integer.parseInt(JOptionPane.showInputDialog("Digite o valor da casa " + ctdr + " do vetor:"));
+        }
+        return vt;
+    }
+
+    public void calcVetor (int[] vt2) {
+        int vtTamanho = vt2.length;
+        for(int ctdr = 0; ctdr < vtTamanho; ctdr ++) {
+            if (vt2[ctdr] % 2 != 0) {
+                System.out.println("O valor: " + vt2[ctdr] + ", é impar.");
+            } else if (vt2[ctdr] % 2 == 0 && vt2[ctdr] % 10 == 0) {
+                System.out.println( "O valor: " + vt2[ctdr] + ", é par e múltiplo de 10.");
+            }
+        }
+        JOptionPane.showMessageDialog(null, "Função concluída, mensagens no console.");
     }
 }
--- Exercicio03/src/view/Principal.java
+++ Exercicio03/src/view/Principal.java
@@ -1,7 +1,28 @@
 package view;
+import javax.swing.JOptionPane;
+import controller.Metodos;
 
 public class Principal {
     public static void main(String[] args) {
-        
+        int codigo = 0;
+        Metodos metodos = new Metodos();
+        int tamanho = metodos.limiteVt();
+        int[] vetor = new int [tamanho];
+
+        while (codigo != 9) {
+            codigo = Integer.parseInt(JOptionPane.showInputDialog("1 - Carregar vetor \n"
+                                                                + "2 - Calcular vetor \n"
+                                                                + "9 - Sair do programa"));
+            switch (codigo) {
+                case 1:
+                    vetor = metodos.carregarVt(vetor);
+                    break;
+                case 2:
+                    metodos.calcVetor(vetor);
+                    break;
+                case 9:
+                    break;
+            }
+        }
     }
 }
