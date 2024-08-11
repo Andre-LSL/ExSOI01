6de655e87b9ff01101f32cc5d95f294664739711
Andre-LSL <andreluizsilvalm@gmail.com>
2024-08-11 17:36:35 -0300

    Resolução - Ex02

--- Exercicio02/src/view/Principal.java
+++ Exercicio02/src/view/Principal.java
@@ -7,8 +7,8 @@ public class Principal {
         int codigo = 0;
         String message;
         while (codigo != 9) {
-            codigo = Integer.parseInt(JOptionPane.showInputDialog("0 - Digitar mensagem (Typing message) (テキストを入力してください) "
-                                                                 + "\n9 - Sair da Aplicação (Exit Aplication) (アプリケーションを終了する) "));
+            codigo = Integer.parseInt(JOptionPane.showInputDialog("0 - Digitar mensagem (Typing message) (テキストを入力してください). "
+                                                                 + "\n9 - Sair da Aplicação (Exit Aplication) (アプリケーションを終了する). "));
             switch (codigo) {
                 case 0:
                 message = JOptionPane.showInputDialog("Digite o texto (Type the text) (テキストを入力してください): ");
