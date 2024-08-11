ba5e196842be86e93de8d247afd88fc4f19834be
Andre-LSL <andreluizsilvalm@gmail.com>
2024-08-11 17:33:23 -0300

    Conlusão/Conclusion - Ex02

--- Exercicio02/src/controller/Metodos.java
+++ Exercicio02/src/controller/Metodos.java
@@ -5,4 +5,14 @@ public class Metodos {
     public Metodos(){
         super();
     }
+
+    public void procedimento (String mensagem) {
+        int contador = 0;
+        String[] lista = mensagem.split(";");
+        for (String palavra : lista) {
+            System.out.println(palavra);
+            contador++;
+        }
+        System.out.println(contador);
+    }
 }
--- Exercicio02/src/view/Principal.java
+++ Exercicio02/src/view/Principal.java
@@ -1,7 +1,22 @@
 package view;
-
+import javax.swing.JOptionPane;
+import controller.Metodos;
 public class Principal {
     public static void main(String[] args) {
-    
+        Metodos metodos = new Metodos();
+        int codigo = 0;
+        String message;
+        while (codigo != 9) {
+            codigo = Integer.parseInt(JOptionPane.showInputDialog("0 - Digitar mensagem (Typing message) (テキストを入力してください) "
+                                                                 + "\n9 - Sair da Aplicação (Exit Aplication) (アプリケーションを終了する) "));
+            switch (codigo) {
+                case 0:
+                message = JOptionPane.showInputDialog("Digite o texto (Type the text) (テキストを入力してください): ");
+                metodos.procedimento(message);
+                    break;
+                case 9:
+                    break;
+            }
+        }
     }
 }
